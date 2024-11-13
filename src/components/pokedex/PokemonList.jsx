0bb9568d977655af7b8a4ac275935c272219fa22
지영 <reizvoll@jiyeong-ui-MacBookAir.local>
2024-11-14 06:31:41 +0900

    README 이미지 수정 및 카드 중앙 정렬 수정

--- src/components/pokedex/PokemonList.jsx
+++ src/components/pokedex/PokemonList.jsx
@@ -3,14 +3,14 @@ import MOCK_DATA from "../../datas/data";
 import PokemonCard from "./PokemonCard";
 import { usePokemonContext } from "../../context/usePokemonContext";
 
-// 리스트 전체 영역
+// 리스트 전체 영역 (가운데 정렬 안맞아서 바꿈 바꿔줘야함)
 const ListContainer = styled.div`
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
   gap: 20px;
-  padding: 20px;
   max-width: 1200px;
-  margin: auto;
+  margin: 20px auto 40px ;
 `;
 
 const PokemonList = () => {
